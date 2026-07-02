773f0987ba53bd53179d86eb1ae91652bfb0d811
Martin Orsag <martin.orsag108@gmail.com>
2026-07-02 14:41:31 +0200

    Testing vitest v2.1

--- apps/frontend/src/app/pages/success/success.spec.ts
+++ apps/frontend/src/app/pages/success/success.spec.ts
@@ -1,15 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Success } from './success';
-import { provideRouter } from '@angular/router';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { OrderService } from '../../services/order-service';
+
+const MOCK_CREATED_ORDER = {
+  id: 'd93bckja0a',
+  userId: 'd93bckja0a',
+  totalAmount: 1,
+  status: 'PENDING',
+  createdAt: new Date(),
+  items: [],
+};
 
 describe('Success', () => {
   let component: Success;
+  let mockOrderService: any;
+  let mockSnapshotRoute: any;
   let fixture: ComponentFixture<Success>;
 
   beforeEach(async () => {
+    mockOrderService = {
+      getOrderById: vi.fn().mockReturnValue(of(MOCK_CREATED_ORDER)),
+    };
+
+    mockSnapshotRoute = {
+      snapshot: {
+        paramMap: convertToParamMap({ id: 'd93bckja0a' }),
+      },
+    };
+
     await TestBed.configureTestingModule({
       imports: [Success],
-      providers: [provideRouter([])],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockSnapshotRoute,
+        },
+        { provide: OrderService, useValue: mockOrderService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Success);
