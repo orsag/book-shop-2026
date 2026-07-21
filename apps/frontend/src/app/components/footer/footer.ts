8c63d5bfafff6680c5dca2695f690680398518c8
Martin Orsag <martin.orsag108@gmail.com>
2026-07-21 10:46:17 +0200

    Footer update

--- apps/frontend/src/app/components/footer/footer.ts
+++ apps/frontend/src/app/components/footer/footer.ts
@@ -3,6 +3,7 @@ import { TranslocoDirective } from '@jsverse/transloco';
 import { RouterLink } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { FOOTER_ITEMS } from '@store/libs';
+import { loremIpsum } from 'lorem-ipsum';
 
 interface FooterItem {
   translationKey: string;
@@ -20,10 +21,27 @@ interface FooterItem {
 export class Footer {
   @ViewChild('wipModal') wipModal!: ElementRef<HTMLDialogElement>;
   selectedItem = signal<FooterItem | null>(null);
+  currentYear: number = new Date().getFullYear();
   footerItems = FOOTER_ITEMS;
+  dummyText: string = "";
+
+  generateDummyText() {
+    return loremIpsum({
+      count: 1, // Number of words, sentences, or paragraphs.
+      format: 'plain', // "plain" or "html".
+      paragraphLowerBound: 4, // Minimum sentences per paragraph.
+      paragraphUpperBound: 8, // Maximum sentences per paragraph.
+      random: Math.random, // PRNG function.
+      sentenceLowerBound: 6, // Minimum words per sentence.
+      sentenceUpperBound: 20, // Maximum words per sentence.
+      suffix: '\n', // Line ending for paragraphs.
+      units: 'paragraphs', // "words", "sentences", or "paragraphs".
+    });
+  }
 
   openWipModal(item: FooterItem) {
     this.selectedItem.set(item);
+    this.dummyText = this.generateDummyText();
     this.wipModal.nativeElement.showModal();
   }
 }
